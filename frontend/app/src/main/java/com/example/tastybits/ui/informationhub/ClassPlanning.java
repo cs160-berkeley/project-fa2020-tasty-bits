7546a97b5f9855388f3ee1baedc9c5ed0eec61b4
Steven <fangchengwang@berkeley.edu>
2020-11-20 15:49:04 -0800

    added navigation drawer and floating button

--- frontend/app/src/main/java/com/example/tastybits/ui/informationhub/ClassPlanning.java
+++ frontend/app/src/main/java/com/example/tastybits/ui/informationhub/ClassPlanning.java
@@ -11,33 +11,51 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.Navigation;
 
 import com.example.tastybits.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ClassPlanning extends Fragment {
 
-    private InfoHubViewModel infoHubViewModel;
+    //private InfoHubViewModel infoHubViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         //infoHubViewModel =ViewModelProviders.of(this).get(InfoHubViewModel.class);
         final View root = inflater.inflate(R.layout.activity_class_planning, container, false);
-        final TextView click = root.findViewById(R.id.id1);
-        final TextView click1 = root.findViewById(R.id.hello);
+        //final TextView click = root.findViewById(R.id.id1);
+        //final TextView click1 = root.findViewById(R.id.hello);
+
+        final DrawerLayout drawer = root.findViewById(R.id.drawerlayout_class_planning);
 
 
-        click.setOnClickListener(new View.OnClickListener() {
+
+        FloatingActionButton expand = root.findViewById(R.id.expand);
+        expand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (drawer.isDrawerOpen(GravityCompat.START)) {
+                    drawer.closeDrawer(GravityCompat.START);
+                } else {
+                    drawer.openDrawer(GravityCompat.START);
+                }
+            }
+        });
+
+        /*click.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ScrollView sv = root.findViewById(R.id.info_scroll);
                 sv.scrollTo((int) click1.getX(), (int) click1.getY());
                 System.out.println("i clicked");
             }
-        });
+        });*/
 
         /*final TextView textView = root.findViewById(R.id.infohub);
         infoHubViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
