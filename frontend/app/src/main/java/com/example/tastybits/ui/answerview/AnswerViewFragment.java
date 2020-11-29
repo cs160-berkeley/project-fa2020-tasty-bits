7350f645f360c28ffe5accbe8daecd00adacca2d
SongwenSu <swsu@ucdavis.edu>
2020-11-29 02:56:11 -0800

    linked with question answer fragment

--- frontend/app/src/main/java/com/example/tastybits/ui/answerview/AnswerViewFragment.java
+++ frontend/app/src/main/java/com/example/tastybits/ui/answerview/AnswerViewFragment.java
@@ -11,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -18,6 +19,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.tastybits.AsyncCallback;
 import com.example.tastybits.NetworkRequest;
 import com.example.tastybits.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 import java.util.ArrayList;
@@ -30,8 +32,8 @@ public class AnswerViewFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View baseView = inflater.inflate(R.layout.fragment_questions_view, container, false);
-        RecyclerView recyclerView = baseView.findViewById(R.id.QuestionsRecyclerView);
+        View baseView = inflater.inflate(R.layout.fragment_answer_view, container, false);
+        RecyclerView recyclerView = baseView.findViewById(R.id.AnswersRecyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         ans_adapter = new AnswerRecyclerViewAdapter(getActivity(), new ArrayList<AnswerItem>());
         recyclerView.setAdapter(ans_adapter);
@@ -49,7 +51,8 @@ public class AnswerViewFragment extends Fragment {
 
             }
         });
-
+        FloatingActionButton addAnswer = baseView.findViewById(R.id.addAnswerButton);
+        addAnswer.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.questionhub_answer));
         return baseView;
     }
 
