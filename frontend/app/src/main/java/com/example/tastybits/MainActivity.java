4d41c30b0c46e149d68c676677dfea034ce938c9
Karim Abedrabbo <karimnabedrabbo@gmail.com>
2020-12-01 14:53:24 -0800

    I forgot to commit these changes I made a while ago as well rip. I'm so forgetful sorry

--- frontend/app/src/main/java/com/example/tastybits/MainActivity.java
+++ frontend/app/src/main/java/com/example/tastybits/MainActivity.java
@@ -35,12 +35,11 @@ public class MainActivity extends AppCompatActivity{
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+        //needs to be first
+        setupLoginManager();
 
         // load the categories
         NetworkRequest.getInstance().queryCategories();
-        setupLoginManager();
-
-
 
     }
 
