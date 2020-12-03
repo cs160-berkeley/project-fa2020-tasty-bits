11abb297c6ac1919a27ba71b2423b4b6fbdf115d
Karim Abedrabbo <karimnabedrabbo@gmail.com>
2020-12-02 21:51:27 -0800

    fixed?

---
+++ frontend/app/src/main/java/com/example/tastybits/ui/loading/LoadingFragment.java
@@ -0,0 +1,49 @@
+package com.example.tastybits.ui.loading;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.GetQuestionsQuery;
+import com.example.GetSuggestedQuestionsQuery;
+import com.example.GetYourQuestionsQuery;
+import com.example.tastybits.AsyncCallback;
+import com.example.tastybits.Constants;
+import com.example.tastybits.LoginManager;
+import com.example.tastybits.MainActivity;
+import com.example.tastybits.NetworkRequest;
+import com.example.tastybits.R;
+import com.example.tastybits.ui.answerview.AnswerItem;
+import com.example.tastybits.ui.answerview.AnswerRecyclerViewAdapter;
+import com.example.tastybits.ui.questionview.QuestionItem;
+import com.example.tastybits.ui.questionview.QuestionRecyclerViewAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoadingFragment extends Fragment {
+    private static final String TAG = "LoadingFragment";
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View baseView = inflater.inflate(R.layout.fragment_loading, container, false);
+
+        return baseView;
+    }
+
+}
